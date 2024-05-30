4b4c117cc846f169bea32a6be414f54aee1b6a00
Mirko R. Aiello <aiello@doneref.com>
2024-05-30 15:09:44 +0200

    Added Singleton

---
+++ Runtime/Singleton/PersistentSingleton.cs
@@ -0,0 +1,10 @@
+﻿namespace Arbinty.Utils
+{
+    public class PersistentSingleton<T> : Singleton<T> where T : Singleton<T>
+    {
+        protected override void OnPostAwake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Runtime/Singleton/Singleton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Arbinty.Utils
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        public static T Instance { get; protected set; }
+
+        public static bool InstanceExists => Instance != null;
+
+        void Awake()
+        {
+            if (InstanceExists)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = (T)this;
+            }
+
+            OnPostAwake();
+        }
+
+        void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+            }
+
+            OnPostDestroy();
+        }
+
+        protected virtual void OnPostAwake() { }
+        protected virtual void OnPostDestroy() { }
+    }
+}
