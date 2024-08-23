6e5a2d6146a652393ddf70ae627addd66bd800ec
Mirko R. Aiello <aiello@doneref.com>
2024-08-23 22:47:15 +0200

    fps

---
+++ Runtime/Hardware/FPSCounter.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using VInspector;
+
+namespace GameUtils
+{
+    public class FPSCounter : Singleton<FPSCounter>
+    {
+        [Tab("Timing")]
+        [SerializeField] private float _resetTime;
+
+        [Tab("Debug")]
+        [SerializeField, ReadOnly] private int _frameCount = 0;
+        [SerializeField, ReadOnly] private float _totalDeltaTime = 0.0f;
+
+        [Space]
+        [SerializeField, ReadOnly] private float _minFPS = float.MaxValue;
+        [SerializeField, ReadOnly] private float _maxFPS = float.MinValue;
+        [SerializeField, ReadOnly] private float _averageFPS = 0.0f;
+
+        //
+        public float MinFPS => _minFPS;
+        public float MaxFPS => _maxFPS;
+        public float AverageFPS => _averageFPS;
+
+        void Update()
+        {
+            // 
+            _totalDeltaTime += Time.deltaTime;
+            _frameCount++;
+
+            // 
+            float currentFPS = 1.0f / Time.deltaTime;
+
+            // 
+            if (currentFPS < _minFPS)
+                _minFPS = currentFPS;
+
+            if (currentFPS > _maxFPS)
+                _maxFPS = currentFPS;
+
+            // 
+            _averageFPS = _frameCount / _totalDeltaTime;
+
+            //
+            if (_totalDeltaTime > _resetTime)
+            {
+                _totalDeltaTime = 0;
+                _frameCount = 0;
+            }
+        }
+    }
+}
