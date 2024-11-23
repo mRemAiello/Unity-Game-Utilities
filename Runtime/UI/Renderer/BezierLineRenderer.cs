8e092562e3b6957f395587fd588833cdb0d7e350
Mirko R. Aiello <aiello@doneref.com>
2024-11-23 22:37:41 +0100

    Bezier

---
+++ Runtime/UI/Renderer/BezierLineRenderer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using VInspector;
+
+namespace GameUtils
+{
+    public class BezierLineRenderer : MonoBehaviour
+    {
+        [SerializeField] private LineRenderer _lineRenderer;
+        [SerializeField] private int _vertexCount = 200;
+        [SerializeField] private Transform _point0;
+        [SerializeField] private Transform _point1;
+        [SerializeField] private Transform _point2;
+
+        //
+        void Start()
+        {
+            _lineRenderer.positionCount = _vertexCount;
+        }
+
+        void Update()
+        {
+            DrawQuadraticBezierCurve();
+        }
+
+        [Button]
+        private void DrawQuadraticBezierCurve()
+        {
+            float t = 0f;
+            Vector3 B = new(0, 0, 0);
+            for (int i = 0; i < _lineRenderer.positionCount; i++)
+            {
+                B = (1 - t) * (1 - t) * _point0.position + 2 * (1 - t) * t * _point1.position + t * t * _point2.position;
+                _lineRenderer.SetPosition(i, B);
+                t += 1 / (float)_lineRenderer.positionCount;
+            }
+        }
+    }
+}
