df85acd835def00a8aca3ff78f6fecb5a2914443
Mirko Raimondo Aiello <aiello@doneref.com>
2025-07-10 14:51:00 +0200

    Update menu item label for Currency editor

--- Editor/Windows/CurrencyEditorWindow.cs
+++ Editor/Windows/CurrencyEditorWindow.cs
@@ -5,7 +5,7 @@ namespace UnityEditor.GameUtils
 {
     public class CurrencyEditorWindow : GenericAssetEditorWindow<CurrencyData>
     {
-        [MenuItem(Constant.MENU_NAME + "Material Editor")]
+        [MenuItem(Constant.MENU_NAME + "Currency Editor")]
         public static void ShowWindow()
         {
             var window = GetWindow<CurrencyEditorWindow>();
