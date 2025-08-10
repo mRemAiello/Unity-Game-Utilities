264fe0a8ba489ee263b90df55ebb600d6960c7ce
Mirko R. Aiello <aiello@doneref.com>
2025-08-10 16:27:49 +0200

    distinzione tra X e Y

--- Runtime/Projectiles/Projectile2D.cs
+++ Runtime/Projectiles/Projectile2D.cs
@@ -15,6 +15,7 @@ namespace GameUtils
         [SerializeField, Group("debug"), ReadOnly] private float _moveSpeed;
         [SerializeField, Group("debug"), ReadOnly] private float _maxMoveSpeed;
         [SerializeField, Group("debug"), ReadOnly] private Vector3 _trajectoryStartPoint;
+        [SerializeField, Group("debug"), ReadOnly] private Vector3 _trajectoryRange;
         [SerializeField, Group("debug"), ReadOnly] private float _trajectoryMaxRelativeHeight;
         [SerializeField, Group("debug"), ReadOnly] private Vector3 _projectileMoveDir;
         [SerializeField, Group("debug"), ReadOnly] private float _nextYTrajectoryPosition;
@@ -64,20 +65,74 @@ namespace GameUtils
 
         private void UpdateProjectilePosition()
         {
-            Vector3 trajectoryRange = _target - _trajectoryStartPoint;
-
-            // 
-            if (trajectoryRange.x < 0)
+            _trajectoryRange = _target - _trajectoryStartPoint;
+            if (Mathf.Abs(_trajectoryRange.normalized.x) < Mathf.Abs(_trajectoryRange.normalized.y))
             {
-                _moveSpeed = -_moveSpeed;
+                // Projectile will be curved on the X axis
+                if (_trajectoryRange.y < 0)
+                {
+                    // Target is located under shooter
+                    _moveSpeed = -_moveSpeed;
+                }
+
+                UpdatePositionWithXCurve();
+            }
+            else
+            {
+                // Projectile will be curved on the Y axis
+                if (_trajectoryRange.x < 0)
+                {
+                    // Target is located behind shooter
+                    _moveSpeed = -_moveSpeed;
+                }
+
+                UpdatePositionWithYCurve();
+            }
+        }
+
+        private void UpdatePositionWithXCurve()
+        {
+            float nextY = transform.position.y + _moveSpeed * Time.deltaTime;
+            float nextPositionYNormalized = (nextY - _trajectoryStartPoint.y) / _trajectoryRange.y;
+
+            //
+            float nextXNormalized = _trajectoryCurve.Evaluate(nextPositionYNormalized);
+            _nextXTrajectoryPosition = nextXNormalized * _trajectoryMaxRelativeHeight;
+
+            //
+            float nextPositionXCorrectionNormalized = _axisCorrectCurve.Evaluate(nextPositionYNormalized);
+            _nextPositionXCorrectionAbsolute = nextPositionXCorrectionNormalized * _trajectoryRange.x;
+
+            //
+            if (_trajectoryRange.x > 0 && _trajectoryRange.y > 0)
+            {
+                _nextXTrajectoryPosition = -_nextXTrajectoryPosition;
             }
 
             //
+            if (_trajectoryRange.x < 0 && _trajectoryRange.y < 0)
+            {
+                _nextXTrajectoryPosition = -_nextXTrajectoryPosition;
+            }
+
+            //
+            float nextPositionX = _trajectoryStartPoint.x + _nextXTrajectoryPosition + _nextPositionXCorrectionAbsolute;
+            Vector3 newPos = new(nextPositionX, nextY, 0);
+            CalculateNextProjectileSpeed(nextPositionYNormalized);
+
+            //
+            _projectileMoveDir = newPos - transform.position;
+            transform.position = newPos;
+        }
+
+        private void UpdatePositionWithYCurve()
+        {
+            //
             float nextX = transform.position.x + _moveSpeed * Time.deltaTime;
-            float nextXNormalized = (nextX - _trajectoryStartPoint.x) / trajectoryRange.x;
+            float nextXNormalized = (nextX - _trajectoryStartPoint.x) / _trajectoryRange.x;
             float nextYNormalized = _trajectoryCurve.Evaluate(nextXNormalized);
             float nextYCorrectionNormalized = _axisCorrectCurve.Evaluate(nextXNormalized);
-            float nextYCorrectionAbsolute = nextYCorrectionNormalized * trajectoryRange.y;
+            float nextYCorrectionAbsolute = nextYCorrectionNormalized * _trajectoryRange.y;
             float nextY = _trajectoryStartPoint.y + nextYNormalized * _trajectoryMaxRelativeHeight + nextYCorrectionAbsolute;
 
             //
